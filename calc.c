909a69a6d013838d0d2adae705237482ca6dca61
tdjol@hotmail.com <Tc3k8-yb46t>
2019-07-06 09:13:59 +0800

    update

--- calc.c
+++ calc.c
@@ -1,10 +1,10 @@
-#include "calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include "calc.h"
 
 enum {
   STRING_CAPACITY = 256
